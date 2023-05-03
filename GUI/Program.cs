c6d8144d9a65f6feddb8025102a072eee695e944
nhithy303 <tranhoangyennhi303@gmail.com>
2023-05-03 15:08:16 +0700

    Change start Form

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -17,7 +17,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmWelcome());
         }
     }
 }
